4122c63fab8b5d9e0e6846aadc040e6ca00769f6
JackyYe <32700242+CNJackyYe@users.noreply.github.com>
2018-04-18 11:20:19 +0800

    Update ItestdemoService.cs

--- RDIFramework.BizLogic/IService/ItestdemoService.cs
+++ RDIFramework.BizLogic/IService/ItestdemoService.cs
@@ -1,4 +1,4 @@
-﻿#region RDIFramework.NET-generated
+#region RDIFramework.NET-generated
 //------------------------------------------------------------------------------
 //     RDIFramework.NET（.NET快速信息化系统开发、整合框架）是基于.NET的快速信息化系统开发、整合框架，给用户和开发者最佳的.Net框架部署方案。
 //     RDIFramework.NET平台包含基础公共类库、强大的权限控制、模块分配、数据字典、自动升级、各商业级控件库、工作流平台、代码生成器、开发辅助
